1f1178a783add73cb5a1b2624ee88ca7c9aa8362
dennis605 <dennis605@web.de>
2025-09-15 16:03:46 +0200

    refactor: Verwalte nvim-config aus lokalem dotfiles-Ordner

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -2,21 +2,14 @@
 { pkgs, ... }:
 
 {
-  # 1. Pakete, die für die Neovim-Konfiguration benötigt werden
   home.packages = [
     pkgs.neovim
     pkgs.git
   ];
 
-  # 2. Die LazyVim Starter-Konfiguration klonen
   home.file.".config/nvim" = {
-    source = pkgs.fetchFromGitHub {
-      owner = "LazyVim";
-      repo = "starter";
-      rev = "main";
-      # Wir verwenden einen leeren Hash, damit Nix uns den richtigen gibt.
-      hash = "sha256-QrpnlDD4r1X4C8PqBhQ+S3ar5C+qDrU1Jm/lPqyMIFM=";
-    };
+    # KORREKTUR: Wir müssen zwei Ebenen nach oben (aus /neovim und aus /modules)
+    source = ../../dotfiles/nvim; 
     recursive = true;
   };
 }
