2deb4277b606539638c3405240f0df7894ccff2f
dennis605 <dennis605@web.de>
2025-09-15 14:23:14 +0200

    gh hinzugefügt

--- flake.nix
+++ flake.nix
@@ -1,38 +1,38 @@
 {
-    description = "Meine erste Nix Konfiguration";
-    
-    inputs = {
-      # Hier kommen später unsere Zutaten (z.B. die Paketsammlung) rein.
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  description = "Meine erste Nix-Konfiguration";
 
-      home-manager = {
+  inputs = {
+    # Wir sagen Nix: "Hole dir die Paketsammlung von diesem GitHub-Repository"
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
       url = "github:nix-community/home-manager";
       # Wichtig: Wir sagen Home Manager, er soll die gleiche nixpkgs-Version
       # benutzen, die wir oben definiert haben. Das verhindert Konflikte.
       inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-      };
+  # Wir übergeben die Inputs (nixpkgs, home-manager) an die Outputs, damit wir sie verwenden können.
+  outputs = { self, nixpkgs, home-manager, ... }: {
 
-    outputs = {self,nixpkgs, home-manager, ... }: {
-      homeConfigurations."linux-user" = home-manager.lib.homeManagerConfiguration {
+    # Wir definieren eine "Home-Konfiguration" und geben ihr einen Namen, z.B. "linux-user"
+    homeConfigurations."linux-user" = home-manager.lib.homeManagerConfiguration {
       
       # Wir müssen angeben, für welches System wir bauen (z.B. ein Linux-PC mit Intel/AMD-CPU)
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      pkgs = nixpkgs.
 
       # Hier kommt die eigentliche Konfiguration für den Benutzer rein.
-      # Wir starten mit einer leeren Liste von Modulen.
+      # Das Modul ist eine Funktion, die 'pkgs' als Argument erhält.
       modules = [
-        {
-          # Hier fügen wir gleich unsere Pakete und Einstellungen ein.
-          # Ersetze die folgenden zwei Zeilen mit deinen echten Daten!
+        ({ pkgs, ... }: {
           home.username = "dennis";
           home.homeDirectory = "/home/dennis";
 
           # Hier ist die Liste der Pakete, die wir installieren wollen.
           home.packages = [
             pkgs.ripgrep  # Wir referenzieren ripgrep aus nixpkgs
+            pkgs.gh
           ];
 
           # Diese Zeile ist wichtig, damit Home Manager sich selbst verwalten kann.
@@ -42,12 +42,9 @@
           # Diese Zeile ist eine Art "Version-Pinning" für deine Konfiguration.
           # Es ist gute Praxis, sie zu setzen.
           home.stateVersion = "23.11";
-         
-          
-        }
+        })
       ];
     };
-     # Hier definieren wir später, was Nix mit den Zutaten machen soll.
+  };
+}
 
-      }
-  }
